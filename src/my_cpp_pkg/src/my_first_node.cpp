81f9217ddcf68bc710a27db51c8c9106c79cd2a7
Tomasz <tzietkowski@gmail.com>
2025-11-28 21:51:20 +0100

    cpp OOP

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,11 +1,34 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode : public rclcpp::Node 
+{
+public:
+    MyNode() : Node("cpp_test"), counter (0)
+    {
+        RCLCPP_INFO(this->get_logger(),"Hello World");
+        timer_ = this->create_wall_timer((std::chrono::seconds{1}),
+                                         std::bind(&MyNode::timerCallback, this));
+    }
+
+private:
+    void timerCallback()
+    {
+        RCLCPP_INFO(this->get_logger(),"Hello %d", counter);
+        counter++;
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter;
+};
+
+
+
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
     //
     auto node = std::make_shared<rclcpp::Node>("cpp_test");
-    RCLCPP_INFO(node->get_logger(),"Hello World");
+    rclcpp::spin(node);
     //
     rclcpp::shutdown();
     return 0;
