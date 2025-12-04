964a359888d12c14efa61a3be9de7aab2cd9cc83
Tomasz <tzietkowski@gmail.com>
2025-12-04 20:43:37 +0100

    add template

---
+++ src/my_py_pkg/my_py_pkg/templete_python_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class MyCustomNode(Node):
+    def __int__(self):
+        super().__init__("node_name")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyCustomNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
